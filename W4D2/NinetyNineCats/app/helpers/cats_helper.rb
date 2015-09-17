4ffa1d2384b7370d7159505d5e71842bf66a0710
Garrett Dycus <gjdycus@gmail.com>
2015-09-16 21:05:19 -0700

    removed w4d2

--- W4D2/NinetyNineCats/app/helpers/cats_helper.rb
+++
@@ -1,5 +0,0 @@
-module CatsHelper
-  def age
-    (Date.today - birth_date).to_i/365
-  end
-end
--- W4D2/NinetyNineCats/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :cats, only: [:index, :show, :new, :create, :edit, :update]
-  resources :cat_rental_requests, only: [:new, :create] do
-    member do
-      post 'approve' => 'cat_rental_requests#approve'
-      post 'deny' => 'cat_rental_requests#deny'
-    end
-  end
-end
