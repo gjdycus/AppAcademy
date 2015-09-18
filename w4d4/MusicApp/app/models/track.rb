9a8c8f600d11ec6c99a6555f9dfe68e4790d1e86
Garrett Dycus <gjdycus@gmail.com>
2015-09-17 23:56:35 -0700

    Completed Music App

---
+++ w4d4/MusicApp/app/models/track.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: tracks
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  album_id   :integer          not null
+#  ttype      :string           not null
+#  lyrics     :text             not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Track < ActiveRecord::Base
+  validates :name, :ttype, :album_id, :lyrics, presence: true
+
+  belongs_to :album,
+    class_name: "Album",
+    foreign_key: :album_id,
+    primary_key: :id
+
+  has_many :notes,
+    -> { order "created_at DESC" },
+    class_name: "Note",
+    foreign_key: :track_id,
+    primary_key: :id,
+    dependent: :destroy
+end
---
+++ w4d4/MusicApp/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resource :session, only: [:create, :destroy, :new]
+
+  resources :users, only: [:create, :new, :show]
+
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, except: [:index, :new] do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, except: [:index, :new] do
+    resources :notes, only: [:create, :destroy]
+  end
+end
